253bfce6506425227137c9b542e3cb3d2bb07acc
Edgar Delgado <delga157@mail.chapman.edu>
2021-01-21 19:49:44 -0800

    Implemented the Events Model

--- SeatGeek/SeatGeek/ViewController.swift
+++ SeatGeek/SeatGeek/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let url = "https://api.seatgeek.com/2/events"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        
+        
+    }
+    
+    func fetchEvents() {
+        
+        
     }
 
+    
 
 }
 
