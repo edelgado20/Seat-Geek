1b554d497768e4a31f7d3d38316a55966fd8992f
Edgar Delgado <delga157@mail.chapman.edu>
2021-01-26 11:03:23 -0800

    Delete boiler plate code from ViewController.swift

--- SeatGeek/SeatGeek/Controllers/ViewController.swift
+++ SeatGeek/SeatGeek/Controllers/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    let apiURL = "https://api.seatgeek.com/2/events?client_id=MjE1MTE5MDl8MTYxMTI4MTg5OC42MDgwMQ"
+    let networkClient = NetworkClient()
     var events: [Event] = [] {
         didSet {
             for event in events {
@@ -24,37 +24,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         
-        fetchEvents { (eventsSummary) in
+        networkClient.fetchEvents{ (eventsSummary) in
             self.events = eventsSummary.events
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
-    }
-    
-    func fetchEvents(completionHandler: @escaping (Events) -> Void) {
-        let url = URL(string: apiURL)!
-
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print("Error fetching the events: \(error)")
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
-                print("Error with the response, unexpected status code: \(String(describing: response))")
-                return
-            }
-
-            if let data = data {
-                let eventsSummary = try! JSONDecoder().decode(Events.self, from: data) //{
-                completionHandler(eventsSummary)
-                    //print("->Events: \(eventsSummary)")
-//                } else {
-//                    print("Wasnt able to decode the events")
-//                }
-            }
-        }
-        task.resume()
     }
     
     func utcToLocal(dateString: String) -> String? {
@@ -94,7 +69,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 160.0
+        return 180.0
     }
 }
 
